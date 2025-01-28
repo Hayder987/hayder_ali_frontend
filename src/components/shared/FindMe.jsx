dd4e85b5a93df96e0def414f180cea3b033eca38
Hayder Ali <hayderbd4290@gmail.com>
2025-01-28 23:30:00 +0600

    new linkedin link added

--- src/components/shared/FindMe.jsx
+++ src/components/shared/FindMe.jsx
@@ -16,7 +16,7 @@ const FindMe = () => {
           <FaGithub />
         </button>
         <button 
-        onClick={()=>window.open("https://www.linkedin.com/in/hayder-ali-9bb52a345/", "_blank", "noopener,noreferrer")}
+        onClick={()=>window.open("https://www.linkedin.com/in/hayder-ali-bb9175349", "_blank", "noopener,noreferrer")}
         className="bg-[#1f2125] hover:text-[#ff014f] rounded-md shadow-[0_4px_10px_rgba(33,31,32,0.8),_0_1px_16px_rgba(89,88,89,0.5)] hover:shadow-[0_4px_24px_rgba(255,1,79,0.8)] duration-300 text-2xl p-3">
           <FaLinkedinIn />
         </button>
